4784962755f0bfd7d557271e3063308a8bf4664a
John A. Lorenc <john.lorenc@gmail.com>
2016-11-17 15:15:00 -0500

    Refactored functional tests with a helper function.

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_quote_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online app with famous quotes. She goes
 		# to check out its homepage
@@ -33,9 +38,7 @@ class NewVisitorTest(unittest.TestCase):
 		# "I think, therefore I am."" as a new quote.
 		inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_element_by_id('id_quote_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('I think, therefore I am.', [row.text for row in rows])
+		self.check_for_row_in_list_table('I think, therefore I am.')
 
 		# There is still a text box inviting her to add another quote. She
 		# enters "Man is a rational animal."
@@ -44,10 +47,8 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		# The page updates again, and now shows both quotes
-		table = self.browser.find_element_by_id('id_quote_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('I think, therefore I am.', [row.text for row in rows])
-		self.assertIn('Man is a rational animal.', [row.text for row in rows])
+		self.check_for_row_in_list_table('I think, therefore I am.')
+		self.check_for_row_in_list_table('Man is a rational animal.')
 
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
