de940630af8ee781c96df9dde8b937dab6c70664
John A. Lorenc <john.lorenc@gmail.com>
2016-11-17 15:45:13 -0500

    Model for quotes and associated migration

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
+from lists.models import Quote
 
 class HomePageTest(TestCase):
 
@@ -27,3 +28,23 @@ class HomePageTest(TestCase):
 		self.assertIn('I think, therefore I am.', response.content.decode())
 		expected_html = render_to_string('home.html', {'new_quote_text': 'I think, therefore I am.'})
 		self.assertEqual(response.content.decode(), expected_html)
+
+class QuoteModelTest(TestCase):
+	
+	def test_saving_and_retrieving_items(self):
+
+		first_quote = Quote()
+		first_quote.text = "I am the alpha and the omega."
+		first_quote.save()
+
+		second_quote = Quote()
+		second_quote.text = "I second that emotion."
+		second_quote.save()
+
+		saved_quotes = Quote.objects.all()
+		self.assertEqual(saved_quotes.count(), 2)
+
+		first_saved_quote = saved_quotes[0]
+		second_saved_quote = saved_quotes[1]
+		self.assertEqual(first_saved_quote.text, "I am the alpha and the omega.")
+		self.assertEqual(second_saved_quote.text, "I second that emotion.")
