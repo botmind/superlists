12048cde94085d4abc967485e916c4bfed9430fc
John A. Lorenc <john.lorenc@gmail.com>
2016-11-24 15:47:53 -0500

    Refactored urls.py

---
+++ lists/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from lists import views
+
+urlpatterns = [
+	url(r'^new$', views.new_quote, name='new_quote'),
+	url(r'^(\d+)/$', views.view_quote, name='view_quote'), #(\d+) captures any digits passed between /.../ and assigns it to an extra param passed to the view function (which we named author_id in views.py)
+    url(r'^(\d+)/add_quote$', views.add_quote, name='add_quote')
+    #url(r'^admin/', include(admin.site.urls)),
+]
--- superlists/urls.py
+++ superlists/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import include, url
-from django.contrib import admin
-from lists import views
+from lists import views as quote_views
+from lists import urls as quote_urls
 
 urlpatterns = [
-	url(r'^$', views.home_page, name='home'),
-	url(r'^quotes/new$', views.new_quote, name='new_quote'),
-	url(r'^quotes/(\d+)/$', views.view_quote, name='view_quote'), #(\d+) captures any digits passed between /.../ and assigns it to an extra param passed to the view function (which we named author_id in views.py)
-    url(r'^quotes/(\d+)/add_quote$', views.add_quote, name='add_quote')
+	url(r'^$', quote_views.home_page, name='home'),
+	url(r'^quotes/', include(quote_urls)),
     #url(r'^admin/', include(admin.site.urls)),
 ]
